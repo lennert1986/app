cb56562a353ca2478d9be6786416c1ebd117f9d3
Nicklas Lennert <33667575+lennert1986@users.noreply.github.com>
2025-02-17 22:50:01 +0100

    Update SmartyPage.php
    Disables user-generated deprecation warnings while keeping all other error reporting settings.
    
    Temp fix

--- lib/Common/SmartyPage.php
+++ lib/Common/SmartyPage.php
@@ -49,6 +49,7 @@ class SmartyPage extends Smarty
         $this->config_dir = $base . 'configs';
         $this->cache_dir = $base . 'cache';
         $this->plugins_dir = $base . 'vendor/smarty/smarty/libs/plugins';
+        $this->error_reporting = error_reporting() & ~E_USER_DEPRECATED; //Deprecated: Using unregistered static method in a template is deprecated and will be removed in a future release. Use Smarty::registerClass to explicitly register a class for access. Future fix needed!
         //$this->error_reporting = E_ALL & ~E_NOTICE;
         $this->muteUndefinedOrNullWarnings();
 
